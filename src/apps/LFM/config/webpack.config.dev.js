4a349e477cf8d38978cb3132f2c5d5bfb0b5c15b
Vassilis Vatikiotis <vvatikiotis@gmail.com>
2018-05-18 14:14:35 +0300

    Add *dev* build for specific app

---
+++ src/apps/LFM/config/webpack.config.dev.js
@@ -0,0 +1,178 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
+const InterpolateHtmlPlugin = require('../../../../config/InterpolateHtmlPlugin');
+const eslintFormatter = require('../../../../config/eslintFormatter');
+const webpack = require('webpack');
+const getClientEnvironment = require('../../../../config//env');
+const path = require('path');
+
+// `publicUrl` is just like `publicPath`, but we will provide it to our app
+// as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
+// Omit trailing slash as %PUBLIC_PATH%/xyz looks better than %PUBLIC_PATH%xyz.
+// const publicUrl = '';
+// Get environment variables to inject into our app.
+// const env = getClientEnvironment(publicUrl);
+
+const paths = {
+  root: path.resolve(__dirname, '../'),
+  dist: path.resolve(__dirname, '../dist'),
+  src: path.resolve(__dirname, '../src'),
+};
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    lfm: path.join(paths.src, 'index.js'),
+  },
+
+  // NOTE: https://webpack.js.org/configuration/output/#module-definition-systems
+  // 'library' definition provides for a script tag usage as well
+  output: {
+    path: paths.dist,
+    filename: '[name].bundle.js',
+  },
+
+  // NOTE: check https://webpack.js.org/configuration/devtool/
+  // and https://github.com/webpack/webpack/tree/master/examples/source-map
+  // and https://lorefnon.me/2016/12/03/on-webpack-and-source-map-integration.html
+  devtool: 'cheap-module-source-map',
+
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+
+  module: {
+    rules: [
+      // First, run the linter.
+      // It's important to do this before Babel processes the JS.
+      {
+        test: /\.(js|jsx|mjs)$/,
+        exclude: /node_modules/,
+        include: paths.appSrc,
+        enforce: 'pre',
+        use: [
+          {
+            loader: require.resolve('eslint-loader'),
+            options: {
+              // formatter: eslintFormatter,
+              eslintPath: require.resolve('eslint'),
+              baseConfig: {
+                extends: [require.resolve('eslint-config-react-app')],
+              },
+              ignore: false,
+              useEslintrc: false,
+            },
+          },
+        ],
+      },
+      {
+        test: /(\.jsx|.js)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              // This is a feature of `babel-loader` for webpack (not Babel itself).
+              // It enables caching results in ./node_modules/.cache/babel-loader/
+              // directory for faster rebuilds.
+              cacheDirectory: true,
+              presets: [
+                'react',
+                'stage-0',
+                'env',
+                //'stage-3',
+                // [
+                //     'env',
+                //     {
+                //         targets: {
+                //             browsers: ['last 3 versions', 'ie >= 10'],
+                //         },
+                //     },
+                // ],
+              ],
+              plugins: [
+                require('babel-plugin-transform-regenerator'),
+                require('babel-plugin-transform-runtime'),
+                require('babel-plugin-transform-class-properties'),
+              ],
+            },
+          },
+        ],
+        exclude: [
+          path.resolve(paths.root, 'node_modules'),
+          path.resolve(paths.root, 'src/apps/LFM/node_modules'),
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader'],
+      },
+      // ** STOP ** Are you adding a new loader?
+      // Make sure to add the new loader(s) before the "file" loader.
+    ],
+  },
+
+  plugins: [
+    new CleanWebpackPlugin([paths.dist]),
+    // Makes some environment variables available in index.html.
+    // The public URL is available as %PUBLIC_URL% in index.html, e.g.:
+    // <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
+    // In development, this will be an empty string.
+    // new InterpolateHtmlPlugin(env.raw),
+    // Generates an `index.html` file with the <script> injected.
+    // new HtmlWebpackPlugin({
+    //   inject: true,
+    //   template: paths.appHtml,
+    // }),
+    // Add module names to factory functions so they appear in browser profiler.
+    new webpack.NamedModulesPlugin(),
+    // This is necessary to emit hot updates (currently CSS only????? CHECK!!):
+    new webpack.HotModuleReplacementPlugin(),
+    // Watcher doesn't work well if you mistype casing in a path so we use
+    // a plugin that prints an error when you attempt to do this.
+    // See https://github.com/facebookincubator/create-react-app/issues/240
+    new CaseSensitivePathsPlugin(),
+  ],
+  // optimization: {
+  //   splitChunks: {
+  //     cacheGroups: {
+  //       ufe: {
+  //         chunks: 'initial',
+  //         test: 'ufe',
+  //         name: 'ufe',
+  //         enforce: true,
+  //       },
+  //       lfm: {
+  //         chunks: 'initial',
+  //         test: 'lfm',
+  //         name: 'lfm',
+  //         enforce: true,
+  //       },
+  //     },
+  //   },
+  // },
+
+  // Some libraries import Node modules but don't use them in the browser.
+  // Tell Webpack to provide empty mocks for them so importing them works.
+  node: {
+    dgram: 'empty',
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    child_process: 'empty',
+  },
+  // Turn off performance hints during development because we don't do any
+  // splitting or minification in interest of speed. These warnings become
+  // cumbersome.
+  performance: {
+    hints: false,
+  },
+  // externals: {
+  //   react: 'commonjs react', // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
+  // },
+};
